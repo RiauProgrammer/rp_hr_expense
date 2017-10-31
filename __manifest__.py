c3ae42b42eb63fbe446ec209a3ef9a6b8a94acd3
mhdsyarif <mhdsyarif.ms@gmail.com>
2017-10-31 18:10:39 +0700

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom Expense',
+    'version': '1.0',
+    'category': 'HR',
+    'summary': 'Modification of hr expense module',
+    'author': 'Muhammad Syarif',
+    'email': 'mhdsyarif.ms@gmail.com',
+    'website': 'http://www.mhdsyarif.com',
+    'maintainer': 'Riau Programmer',
+    'license': 'AGPL-3',
+    'depends': ['hr_expense'],
+    'data': [
+        'views/expense_view.xml',
+    ],
+    'images': ['static/description/syarif.png'],
+    'installable': True,
+    'auto_install': False
+}
